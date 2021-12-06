655785832a4409ff025e1767fe6382502cbb8e93
LuciusBossio <lucius.bossio@gmail.com>
2021-12-06 13:51:26 -0500

    Wash Controller/Entity/Service/Repo/Schema/Data set up

---
+++ src/main/java/comp31/dachopwash/models/entities/Wash.java
@@ -0,0 +1,34 @@
+package comp31.dachopwash.models.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="WASH")
+public class Wash {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="WASH_ID")
+    private Integer washID;
+    
+    @Column(name="WASH_TYPE")
+    private Integer washType;
+
+    @Column(name="WASH_STATUS")
+    private Integer washStatus;
+
+    @OneToOne(mappedBy = "wash")
+    private Employee employee;
+
+    @OneToOne(mappedBy = "wash")
+    private Customer customer;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO WASH (WASH_ID, WASH_TYPE, WASH_STATUS, EMPLOYEE_ID, CUSTOMER_ID) VALUES 
+(NULL, 'basic', 1, 01, 05),
+(NULL, 'basic', 0, 02, 15),
+(NULL, 'basic', 0, 01, 20),
+(NULL, 'premium', 0, 02, 25),
+(NULL, 'executive', 0, 04, 30),
+(NULL, 'executive', 0, 05, 35);
