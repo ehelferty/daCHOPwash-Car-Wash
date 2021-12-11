1ce3747d24d60b6cb5820e10608667827e14c4ac
Graham Corcoran <graham@corcoran.me>
2021-12-11 13:40:56 -0500

    Added blank invoices page.

--- src/main/java/comp31/dachopwash/controllers/InvoiceController.java
+++ src/main/java/comp31/dachopwash/controllers/InvoiceController.java
@@ -1,8 +1,15 @@
 package comp31.dachopwash.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/invoices")
 public class InvoiceController {
-    
+    @GetMapping("")
+    public String getInvoices()
+    {
+        return "invoices";
+    }
 }
