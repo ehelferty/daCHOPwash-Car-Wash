fda38913bb1a057c59ae05599dff0c395b0f425b
LuciusBossio <lucius.bossio@gmail.com>
2021-12-13 02:51:22 -0500

    wash controller test commit

--- src/main/java/comp31/dachopwash/controllers/WashController.java
+++ src/main/java/comp31/dachopwash/controllers/WashController.java
@@ -3,22 +3,23 @@ package comp31.dachopwash.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import comp31.dachopwash.models.entities.Wash;
 import comp31.dachopwash.services.WashService;
 
 @Controller
 public class WashController {
+    WashService washService;
 
-    @Autowired WashService washService;
-
-    @GetMapping("/washes/book")
-    public String bookWash(@ModelAttribute Wash wash, Model model)
-    {
-
-        return "book";
-    } 
+    @Autowired
+    public WashController(WashService washService) {
+        super();
+        this.washService = washService;
+    }
     
+    @GetMapping("/wash")
+    public String getWash(Model model) {
+        model.addAttribute("wash", washService.findWashes());
+        return "wash";
+    }
 }
