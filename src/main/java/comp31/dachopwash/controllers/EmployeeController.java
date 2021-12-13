255cee0f51948fc69aff149184cdcca7247f4b0f
ehelferty <eric.helferty@gmail.com>
2021-12-13 10:04:41 -0500

    Minor html changes

--- src/main/java/comp31/dachopwash/controllers/EmployeeController.java
+++ src/main/java/comp31/dachopwash/controllers/EmployeeController.java
@@ -1,17 +1,13 @@
 package comp31.dachopwash.controllers;
 
-import java.time.LocalDate;
 
-import org.hibernate.query.criteria.internal.expression.function.CurrentDateFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import comp31.dachopwash.models.entities.Employee;
